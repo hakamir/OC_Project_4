1faa59d03ae3a76d097fd4ee9617e909ac752fce
Marie Donnet <marie.donnet@laposte.net>
2019-01-21 16:48:46 +0100

    Update appli.py
    Onglet 1 et 2

--- appli.py
+++ appli.py
@@ -1,44 +1,26 @@
-from PyQt5 import QtWidgets, QtCore, QtWebEngineWidgets 
-
+import sys
+from itertools import product
+from PyQt5.QtWidgets import QMainWindow, QApplication,QHBoxLayout, QFrame,QPushButton,QTableWidgetItem, QWidget, QAction, QTabWidget,QVBoxLayout,QLabel,QTableWidget
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+from PyQt5.QtWebEngineWidgets import *
+from pylab import *
+from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
 from gmplot import gmplot
-
 import os
 
 
-class MainWindow(QtWidgets.QMainWindow): 
+class MainWindow(QMainWindow): 
     def __init__(self, parent = None): 
      super(MainWindow, self).__init__() 
      self.__setup__() 
 
     def __setup__(self): 
-     self.resize(600, 600) 
-     tabWidget = TabWidget(self) 
-     self.setCentralWidget(tabWidget) 
+     self.resize(1000, 600) 
+     tabs = TabWidget(self) 
+     self.setCentralWidget(tabs) 
 
-    """ options = Options(self) 
-     optionsDock = QtWidgets.QDockWidget() 
-     optionsDock.setWidget(options) 
-     optionsDock.setWindowTitle("Options") 
-     self.addDockWidget(QtCore.Qt.TopDockWidgetArea, optionsDock) 
-
-     options.spinBox_columns.valueChanged.connect(lambda: tabWidget.tabWidget.currentWidget(). 
-                setColumnCount(options.spinBox_columns.value())) """
-"""
-class Options(QtWidgets.QWidget): 
-    def __init__(self, parent): 
-     super(Options, self).__init__(parent) 
-     self.__setup__() 
-
-    def __setup__(self): 
-     self.spinBox_columns = QtWidgets.QSpinBox() 
-     self.spinBox_columns.setValue(1) 
-     self.spinBox_columns.setMinimum(1) 
-
-     layout = QtWidgets.QVBoxLayout() 
-     layout.addWidget(self.spinBox_columns) 
-     self.setLayout(layout) """
-
-class TabWidget(QtWidgets.QWidget): 
+class TabWidget(QWidget): 
     def __init__(self, parent): 
      super(TabWidget, self).__init__(parent) 
      self.__setup__() 
@@ -46,27 +28,57 @@ class TabWidget(QtWidgets.QWidget):
      #map.__setup__()
      
     def __setup__(self): 
-        self.tabWidget = QtWidgets.QTabWidget() 
-        
-        self.view = QtWebEngineWidgets.QWebEngineView()
-        self.tabWidget.addTab(self.view, "Map") 
+        # Initialize tab screen
+        self.tabs = QTabWidget() 
+        self.tab1 = QWebEngineView()
+        self.tab2 = QTableWidget() 
+        self.tab3 = QTableWidget()
 
-        widget_2 = QtWidgets.QTableWidget() 
-        self.tabWidget.addTab(widget_2, "Sys2") 
+        # Add tabs
+        self.tabs.addTab(self.tab1, "Map") 
+        self.tabs.addTab(self.tab2, "Cardio")
+        self.tabs.addTab(self.tab3, "Domotique") 
         
-        widget_3 = QtWidgets.QTableWidget() 
-        self.tabWidget.addTab(widget_3, "Sys3") 
-        
-        layout = QtWidgets.QVBoxLayout() 
-        layout.addWidget(self.tabWidget) 
+        # Create first tab Map
+        layout = QVBoxLayout() 
+        layout.addWidget(self.tabs) 
         self.setLayout(layout) 
         fichierweb = "file:///" + os.path.abspath("my_map.html").replace("\\", "/") + "#partiecommune"
-        self.page = QtWebEngineWidgets.QWebEnginePage()
-        self.page.setUrl(QtCore.QUrl(fichierweb))
-        self.view.setPage(self.page)
-        self.view.show()
+        self.page = QWebEnginePage()
+        self.page.setUrl(QUrl(fichierweb))
+        self.tab1.setPage(self.page)
+        self.tab1.show()
+
+        # Create second tab Cardio
+        self.tab2.principalLayout = QHBoxLayout(self.tab2)
+
+        self.tab2.rightFrame = QFrame(self.tab2)
+        self.tab2.verticalLayout = QVBoxLayout(self.tab2.rightFrame)
+        self.table1 = QTableWidget()
+        self.table1.setRowCount(150)
+        self.table1.setColumnCount(2)
+        self.table1.setHorizontalHeaderLabels(["Heure","Rythme Cardiaque"])
+        self.tab2.verticalLayout.addWidget(self.table1)
+        self.tab2.principalLayout.addWidget(self.tab2.rightFrame)
+
+
+        self.tab2.verticalLayoutR = QVBoxLayout()
+        self.tab2.verticalLayoutR.setSpacing(0)
+        self.tab2.exitFrame = QFrame(self.tab2)
+
+        self.fig = Figure()
+        self.axes = self.fig.add_subplot(111)
+ 
+        self.x = linspace(-pi, pi, 30)
+        self.y = cos(self.x)
+        self.line, = self.axes.plot(self.x, self.y)
+
+        self.canvas = FigureCanvas(self.fig)
+        self.tab2.verticalLayoutR.addWidget(self.canvas)
+
+        self.tab2.principalLayout.addLayout(self.tab2.verticalLayoutR)
      
-class Map(QtWidgets.QWidget):
+class Map(QWidget):
 
     def __init__(self):
         super().__init__()
@@ -79,7 +91,7 @@ class Map(QtWidgets.QWidget):
 
 def main(): 
     import sys 
-    app = QtWidgets.QApplication(sys.argv) 
+    app = QApplication(sys.argv) 
     window = MainWindow() 
     window.show() 
     app.exec_()
