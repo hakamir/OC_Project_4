ecc70450350d00cd1519dc085a7bc68621b61b35
Marie Donnet <marie.donnet@laposte.net>
2019-01-23 12:20:45 +0100

    comm xbee

--- check_authorisation.py
+++ check_authorisation.py
@@ -3,37 +3,23 @@ import csv
 from datetime import datetime
 
 idRFID = 0
-temperature = -1
 
 with open('autorisations.json') as file:
     json_data = json.load(file)
-    #print(json_data)
-    #print(json.dumps(json_data, sort_keys=True, indent=4 * ' '))
 
-def ecrireLog(id, prenom, nom, autorisation):
-    with open('log.csv', 'a', newline='') as csvfile:
-        writer = csv.writer(csvfile)
-        writer.writerow([datetime.now().strftime('%d/%m/%Y-%H:%M:%S'),temperature, id, prenom, nom, autorisation])
-
-def ecrireLogInconnu():
-    ecrireLog('inconnu', 'inconnu', 'inconnu', 'inconnu')
-
-def nbPersonnes():
+def nbPersonnes(tmp):
     for personne in json_data['Personnes']:
         ID = personne['ID']
 
-        if ID == idRFID:
-            ecrireLog(idRFID, personne['prenom'], personne['nom'], personne['autorisation'])
+        if ID == tmp:
 
             if personne['nb_personnes'] == '1':
                 return 1
-            else if personne['nb_personnes'] == '2':
+            elif personne['nb_personnes'] == '2':
                 return 2
-            else if personne['nb_personnes'] == '3':
+            elif personne['nb_personnes'] == '3':
                 return 3
 
-    #Le TAG ne correspond à aucune personnes du fichier JSON
-    ecrireLogInconnu()
     return False
 
 
---
+++ xbee.py
@@ -0,0 +1,47 @@
+import serial
+import check_authorisation as check
+
+port = serial.Serial('/dev/tty.usbserial-A506QTYE', 9600)
+
+print("Port {} ouvert".format(port.name))
+print("*****************************")
+
+#def nbPers()
+while True:
+        line = port.readline()
+        if not line:
+            continue
+
+        try:
+            splitLine = line.decode().split(' ')
+        except UnicodeDecodeError:
+            continue
+        
+        #print(splitLine)
+
+        while splitLine[0] == "ISO15693":
+            try:
+                tmp = splitLine[2]
+            except IndexError:
+                continue
+            tmp = tmp.replace('[', '')
+            tmp = tmp.replace(']', '')
+            tmp = tmp.replace('\n', '')
+            tmp = tmp.replace('\r', '')
+            print(tmp)  
+
+            nb_personnes = check.nbPersonnes(tmp)
+            print('Nombres de personnes autorisees : ' + str(nb_personnes))
+        
+            line = port.readline()
+            if not line:
+                break
+            try:
+                splitLine = line.decode().split(' ')
+            except UnicodeDecodeError:
+                continue
+        
+
+port.close()
+print("*****************************")
+print("Port fermé")
